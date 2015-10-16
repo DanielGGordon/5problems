75a91ea4bbed3b79363bc7de36cff62b950f197f
Daniel Gordon <Daniel.gordon@here.com>
2015-10-16 10:59:06 -0500

    Reworked structure to use directories

---
+++ 1/SumList.java
@@ -0,0 +1,8 @@
+// ##Problem 1
+
+// Write three functions that compute the sum of the numbers in a given 
+// list using a for-loop, a while-loop, and recursion.
+
+public static void main(String[] args) {
+        
+}
